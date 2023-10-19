79db7af023b8a47fb7a2de9a86192fea4acc36f2
huitoutunao <lh_huitoutunao@163.com>
2023-10-19 22:54:23 +0800

    feat: 动态列表

--- react-staging/src/components/Item/index.jsx
+++ react-staging/src/components/Item/index.jsx
@@ -3,13 +3,14 @@ import './index.css'
 
 export default class Item extends Component {
   render() {
+    const { name, done } = this.props
     return (
       <li>
         <label>
-            <input type="checkbox" />
-            <span>xxxx</span>
+            <input type="checkbox" defaultChecked={ done } />
+            <span>{ name }</span>
         </label>
-        <button className="btn btn-danger">删除</button>
+        <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
       </li>
     )
   }
--- react-staging/src/components/List/index.jsx
+++ react-staging/src/components/List/index.jsx
@@ -4,9 +4,14 @@ import './index.css'
 
 export default class List extends Component {
   render() {
+    const { todos } = this.props
     return (
       <ul className="todo-main">
-        <Item />
+        {
+          todos.map((item) => {
+            return <Item key={ item.id } { ...item } />
+          })
+        }
       </ul>
     )
   }
