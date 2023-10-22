5c96bfaa03e666f9353bbe3ea3d10e90039244c3
huitoutunao <lh_huitoutunao@163.com>
2023-10-22 22:48:46 +0800

    feat: 新增配置代理

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+  getStudents = () => {
+    axios.get('/students').then(
+      (res) => {
+        console.log('成功了', res.data)
+      },
+      (err) => {
+        console.log('失败了', err)
+      }
+    )
+  }
+
   render() {
     return (
-      <div>App</div>
+      <button onClick={ this.getStudents }>点我获取学生数据</button>
     )
   }
 }
