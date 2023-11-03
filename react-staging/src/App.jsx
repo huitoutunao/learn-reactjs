b9e22d5809e26c6d310614fcf00138d106556f9d
huitoutunao <lh_huitoutunao@163.com>
2023-11-03 23:04:54 +0800

    feat: 创建单页应用

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import Header from './components/Header'
-import List from './components/List'
 
 export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <Header></Header>
-        <List></List>
+        app
       </div>
     )
   }
