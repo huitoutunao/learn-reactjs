c9ccc27af2a4aeeb1552343a79f418777426cb4a
huitoutunao <lh_huitoutunao@163.com>
2023-11-20 23:31:19 +0800

    feat: react-redux的基本使用

---
+++ react-staging/24_src_react-redux的基本使用/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import store from './redux/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// 全局订阅
+store.subscribe(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
