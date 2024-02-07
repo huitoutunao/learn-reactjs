159286946ad8a3787a185dc5c5359e8b06212b9d
huitoutunao <lh_huitoutunao@163.com>
2024-02-07 22:31:59 +0800

    feat: 新建一个空白页面

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,54 +1,7 @@
 import React from 'react'
-import { NavLink, useRoutes, useNavigate, useInRouterContext } from 'react-router-dom'
-import routes from './routes'
-// import Home from './pages/Home'
-// import About from './pages/About'
 
 export default function App() {
-  const element = useRoutes(routes)
-  console.log('useInRouterContext', useInRouterContext())
-  const navigate = useNavigate()
-  const back = () => {
-    navigate(-1)
-  }
-  const forward = () => {
-    navigate(1)
-  }
-
   return (
-    <div>
-        <div className='row'>
-          <div className='col-xs-offset-2 col-xs-8'>
-            <div className='page-header'>
-              <h2>React Router Demo</h2>
-              <button onClick={forward}>前进</button>
-              <button onClick={back}>后退</button>
-            </div>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-2'>
-            <div className='list-group'>
-
-              {/* 原生 html 中，靠 <a> 标签跳转不同的页面 */}
-              {/* <a className='list-group-item active' href='./home.html'>Home</a>
-              <a className='list-group-item' href='./about.html'>About</a> */}
-
-              {/* 路由链接 */}
-              <NavLink className="list-group-item" to='./home'>Home</NavLink>
-              <NavLink className="list-group-item" to='./about'>About</NavLink>
-
-            </div>
-          </div>
-          <div className='col-xs-6'>
-            <div className='panel'>
-              <div className='panel-body'>
-                {/* 注册路由 */}
-                {element}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>App</div>
   )
 }
--- react-staging/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <div>我是About内容</div>
-  )
-}
