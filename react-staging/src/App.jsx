d45735fd219f0ca432cee85fe788f68ff77bcf27
huitoutunao <lh_huitoutunao@163.com>
2024-03-24 23:23:20 +0800

    feat: demo

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,16 +1,25 @@
-import { useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function App() {
-  const myRef = useRef(null)
+  const [count, setCount] = useState(0)
+  const newCount = `${count}次`
 
-  function handleDom() {
-    console.log(myRef.current)
+  useEffect(() => {
+    // 页面挂载完执行
+    return () => {
+      // 清理函数
+    }
+  }, [])
+
+  const handleCount = () => {
+    setCount(count + 1)
   }
 
   return (
     <>
-      <div ref={myRef}>App</div>
-      <button onClick={handleDom}>DOM</button>
+      <div>{count}</div>
+      <p>{newCount}</p>
+      <button onClick={handleCount}>click</button>
     </>
   )
 }
