6d89ce4dc5002f25cbec42874f784fe24a990bf4
huitoutunao <lh_huitoutunao@163.com>
2025-02-24 16:51:28 +0800

    feat: 删减无关文件

--- react-staging/src/App.jsx
+++ react-staging/src/App.jsx
@@ -1,10 +1,7 @@
-import style from './styles/index.module.css'
-import other from './styles/other.module.scss'
 export default function App() {
   return (
     <>
-      <div className={style.text + ' text_bg'}>我是App内容</div>
-      <div className={other.btn}>按钮</div>
+      <div>App</div>
     </>
   )
 }
--- react-staging/src/components/Son.jsx
+++ react-staging/src/components/Son.jsx
@@ -1,8 +1,9 @@
-import { memo, useContext } from 'react'
-import { ThemeContext } from '../App'
-
-function ConsumerDemo() {
-  const { color, background } = useContext(ThemeContext)
-  return <div style={{ color, background }}>消费者</div>
+function Son() {
+  return (
+    <>
+      <div> i am alien </div>
+    </>
+  )
 }
-export default memo(() => <ConsumerDemo />)
+
+export default Son
